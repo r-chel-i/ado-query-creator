8bbb8f6214de7a5b251191d913d9791306592fb9
Rachel <150952786+r-chel-i@users.noreply.github.com>
2025-12-22 18:59:05 +0000

    Updated Dashboard image link

--- client/components/DashboardImage.tsx
+++ client/components/DashboardImage.tsx
@@ -1,11 +1,11 @@
 export function DashboardImage() {
   return (
-    <section className="px-4 py-8">
+    <section className="px-8 py-8">
       <div className="max-w-6xl mx-auto">
         <div className="relative flex justify-center">
           {/* Dashboard image with direct styling */}
           <img
-            src="https://venturebeat.com/wp-content/uploads/2018/09/boards2x.png"
+            src="https://miro.medium.com/v2/resize:fit:1400/0*im6JLx7QMRVleoRb.png"
             alt="Azure DevOps Dashboard Interface"
             className="block max-w-full h-auto rounded-xl shadow-2xl"
           />
